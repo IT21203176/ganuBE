3f53cb9d711049b772120da14358ab0e2ad4691b
Janithya <janithya@corali.tech>
2025-10-25 20:12:49 +0530

    vercel

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     //origin: "https://die-vehicle-taxation-fe.vercel.app",
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: "https://ganu-fe.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
